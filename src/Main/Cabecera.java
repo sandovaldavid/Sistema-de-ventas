a5b0a27a9efb50c4c8128bbc45d759eeefe2fd2b
sandovaldavid <sandovaldavid2201@gmai.com>
2023-07-07 11:57:05 -0500

    Estructura del Sistema-de-ventas

---
+++ src/Main/Cabecera.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Main;
+
+/**
+ *
+ * @author David Sandoval Salvador
+ * @E-mail sandovaldavid2201@gmail.com
+ * @Fecha 7 jul. 2023
+ *
+ */
+public class Cabecera {
+
+}
