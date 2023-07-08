268fc06445d6aac572e84f66a50afc48d198381d
sandovaldavid <sandovaldavid2201@gmai.com>
2023-07-08 10:40:47 -0500

    creamos los contructores y los metodos set y get de la clase Header

--- src/Main/Header.java
+++ src/Main/Header.java
@@ -4,6 +4,9 @@
  */
 package Main;
 
+import java.io.IOException;
+import java.io.RandomAccessFile;
+
 /**
  *
  * @author David Sandoval Salvador
@@ -11,6 +14,75 @@ package Main;
  * @Fecha 7 jul. 2023
  *
  */
-public class Cabecera {
+public class Header {
 
+    private RandomAccessFile IACab;
+    private long RecordsNumber;         //long 8 bytes
+    private long RecordsNumberDeleted;  //long 8 bytes
+    private int RecordSize;             //int  4 bytes
+    //                                  ----------------
+    //                                  total = 20 bytes
+
+    public Header() {
+        this.RecordsNumber = 0;
+        this.RecordSize = 0;
+        this.RecordsNumberDeleted = 0;
+    }
+
+    public RandomAccessFile getIACab() {
+        return IACab;
+    }
+
+    public void setIACab(RandomAccessFile IACab) {
+        this.IACab = IACab;
+    }
+
+    public long getRecordsNumber() {
+        return RecordsNumber;
+    }
+
+    public void setRecordsNumber(long RecordsNumber) {
+        this.RecordsNumber = RecordsNumber;
+    }
+
+    public int getRecordSize() {
+        return RecordSize;
+    }
+
+    public void setRecordSize(int RecordSize) {
+        this.RecordSize = RecordSize;
+    }
+
+    public long getRecordsNumberDeleted() {
+        return RecordsNumberDeleted;
+    }
+
+    public void setRecordsNumberDeleted(long RecordsNumberDeleted) {
+        this.RecordsNumberDeleted = RecordsNumberDeleted;
+    }
+
+    public int getSize() {
+        return 20;
+    }
+
+    @Override
+    public String toString() {
+        return "Header{" + "IACab=" + IACab + ", RecordsNumber=" + RecordsNumber + ", RecordSize=" + RecordSize + ", RecordsNumberDeleted=" + RecordsNumberDeleted + '}';
+    }
+
+    public void position() throws IOException {
+        IACab.seek(0);
+    }
+
+    public void Write() throws IOException {
+        getIACab().writeLong(getRecordsNumber());
+        getIACab().writeLong(getRecordsNumberDeleted());
+        getIACab().writeInt(getRecordSize());
+    }
+
+    public void Read() throws IOException {
+        setRecordsNumber(getIACab().readLong());
+        setRecordsNumberDeleted(getIACab().readLong());
+        setRecordSize(getIACab().readInt());
+    }
 }
