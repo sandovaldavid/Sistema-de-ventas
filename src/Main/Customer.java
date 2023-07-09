cf0156f0a33af38aee5f033ea3923af193bd69a7
sandovaldavid <sandovaldavid2201@gmai.com>
2023-07-08 23:10:29 -0500

    Renombre y elimine algunos atributos redundantes.

--- src/Main/Customer.java
+++ src/Main/Customer.java
@@ -4,6 +4,7 @@
  */
 package Main;
 
+import java.io.EOFException;
 import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
@@ -21,31 +22,27 @@ import java.util.logging.Logger;
 public class Customer extends Archivo {
 
     private String DNI;                 //Char(8)  --> 10 bytes
-    private String FirstName;           //Char(20) --> 22 bytes
-    private String SecondName;          //Char(20) --> 22 bytes
-    private String FirstLastName;       //Char(30) --> 32 bytes
-    private String SecondLastName;      //Char(30) --> 32 bytes
+    private String Name;                //Char(30) --> 42 bytes
+    private String LastName;            //Char(50) --> 52 bytes
     private String Nationality;         //Char(20) --> 22 bytes
     private String PhoneNumber;         //Char(9)  --> 11 bytes
     private String CellphoneNumber;     //Char(9)  --> 11 bytes
     private String Addrees;             //Char(40) --> 42 bytes
     private String RUC;                 //Char(10) --> 12 bytes
     private int Age;                    //int      --> 4  bytes
-    private Date BirthdayDate;          //Date     --> 20 bytes (save the date like string.)
+    private Date BirthdayDate;          //Date     --> 22 bytes (save the date like string.)
     private byte Active;                //byte     --> 1  byte  (1 -> true and 0 -> false)
     //-------------------------
-    //Total    --> 241 bytes
+    //Total    --> 229 bytes
 
     public Customer(String FileName, String FileExtension) {
         super(FileName, FileExtension);
     }
 
-    public Customer(String DNI, String FirstName, String SecondName, String FirstLastName, String SecondLastName, String Nationality, String PhoneNumber, String CellphoneNumber, String Addrees, String RUC, int Age, Date BirthdayDate) {
+    public Customer(String DNI, String Name, String LastName, String Nationality, String PhoneNumber, String CellphoneNumber, String Addrees, String RUC, int Age, Date BirthdayDate) {
         this.DNI = DNI;
-        this.FirstName = FirstName;
-        this.SecondName = SecondName;
-        this.FirstLastName = FirstLastName;
-        this.SecondLastName = SecondLastName;
+        this.Name = Name;
+        this.LastName = LastName;
         this.Nationality = Nationality;
         this.PhoneNumber = PhoneNumber;
         this.CellphoneNumber = CellphoneNumber;
@@ -64,36 +61,20 @@ public class Customer extends Archivo {
         this.DNI = DNI;
     }
 
-    public String getFirstName() {
-        return FirstName;
+    public String getName() {
+        return Name;
     }
 
-    public void setFirstName(String FirstName) {
-        this.FirstName = FirstName;
+    public void setName(String Name) {
+        this.Name = Name;
     }
 
-    public String getSecondName() {
-        return SecondName;
+    public String getLastName() {
+        return LastName;
     }
 
-    public void setSecondName(String SecondName) {
-        this.SecondName = SecondName;
-    }
-
-    public String getFirstLastName() {
-        return FirstLastName;
-    }
-
-    public void setFirstLastName(String FirstLastName) {
-        this.FirstLastName = FirstLastName;
-    }
-
-    public String getSecondLastName() {
-        return SecondLastName;
-    }
-
-    public void setSecondLastName(String SecondLastName) {
-        this.SecondLastName = SecondLastName;
+    public void setLastName(String LastName) {
+        this.LastName = LastName;
     }
 
     public String getNationality() {
@@ -161,7 +142,7 @@ public class Customer extends Archivo {
     }
 
     public int getSize() {
-        return 241;
+        return 227;
     }
 
     public String getBirthdayDateString() {
@@ -195,27 +176,23 @@ public class Customer extends Archivo {
     @Override
     public void Write() throws IOException {
         getIA().writeUTF(String.format("%8.8s", getDNI()));
-        getIA().writeUTF(String.format("%30.30s", getFirstName()));
-        getIA().writeUTF(String.format("%30.30s", getSecondName()));
-        getIA().writeUTF(String.format("%40.40s", getFirstLastName()));
-        getIA().writeUTF(String.format("%40.40s", getSecondLastName()));
+        getIA().writeUTF(String.format("%30.30s", getName()));
+        getIA().writeUTF(String.format("%40.40s", getLastName()));
         getIA().writeUTF(String.format("%20.20s", getNationality()));
         getIA().writeUTF(String.format("%9.9s", getPhoneNumber()));
         getIA().writeUTF(String.format("%9.9s", getCellphoneNumber()));
         getIA().writeUTF(String.format("%40.40s", getAddrees()));
         getIA().writeUTF(String.format("%10.10s", getRUC()));
         getIA().writeInt(getAge());
-        getIA().writeUTF(String.format("%8.8s", getBirthdayDateString()));
+        getIA().writeUTF(String.format("%20.20s", getBirthdayDateString()));
         getIA().writeByte(getActive());
     }
 
     @Override
     public void Read() throws IOException {
         setDNI(getIA().readUTF().trim());
-        setFirstName(getIA().readUTF().trim());
-        setSecondName(getIA().readUTF().trim());
-        setFirstLastName(getIA().readUTF().trim());
-        setSecondLastName(getIA().readUTF().trim());
+        setName(getIA().readUTF().trim());
+        setLastName(getIA().readUTF().trim());
         setNationality(getIA().readUTF().trim());
         setPhoneNumber(getIA().readUTF().trim());
         setCellphoneNumber(getIA().readUTF().trim());
